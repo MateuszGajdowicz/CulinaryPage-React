c004bded0119757b2e13c6483dcfec2189cbc1f1
MateuszGajdowicz <155658500+MateuszGajdowicz@users.noreply.github.com>
2025-04-14 01:50:23 +0200

    commit bo moge

--- my-react-app/src/assets/MainPage.jsx
+++ my-react-app/src/assets/MainPage.jsx
@@ -8,24 +8,51 @@ import udon from './images/udon.png';
 import './MainPage.css';
 
 function MainPage(){
-    const [recipe, setRecipe] = useState(["Spaghetti", "Pizza", "Lasagne", "Zupa Pomidorowa", "Strogonov"]);
     const BackgroundList = [pizza, macand, chicken, ratat, udon];
 
     const [backgroundDish, setBackgroundDish] = useState(BackgroundList[0])
-    
+    const [recipes, setRecipes]= useState([]);
+    const [allRecipes, setAllRecipes]= useState([]);
 
+    useEffect(()=>{
+        async function GetFoodApi(){
+            const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?number=400&apiKey=4da36d53f6964c109acd650a0a6d7d33");
+            const data = await response.json();
+            setRecipes(data.results)
+            setAllRecipes(data.results)
+
+        }
+        GetFoodApi();
+    },[])
+    
     useEffect(()=>{
         const BackgroundInterval = setInterval(() => {
             const randomBackgroundIndex = Math.floor(Math.random()*BackgroundList.length);
             setBackgroundDish(BackgroundList[randomBackgroundIndex])
-            console.log(randomBackgroundIndex)
             
         }, 3000);
-        return () => clearInterval(BackgroundInterval); // czyszczenie interwału
+        return () => clearInterval(BackgroundInterval); 
 
         
     },[])
     
+    const [recipeName, setRecipeName] = useState();
+
+    function SearchFood(){
+        if(recipeName){
+            const SearchedFood = recipes.filter(element=>element.title.toLowerCase().includes(recipeName.toLowerCase()));
+            setRecipes(SearchedFood);
+
+        }
+        else{
+            setRecipes(allRecipes)
+        }
+    }
+    useEffect(()=>{
+        if(recipeName===""){
+            setRecipes(allRecipes)
+        }
+    },[recipeName])
     return(
 
         
@@ -55,12 +82,12 @@ function MainPage(){
         <div style={{backgroundImage:`url(${backgroundDish})`}} className='BackgroundContainer'></div>
         <div className='RecipesContainer'>
             <h1>Popularne Przepisy</h1>
-            <input type="text" placeholder='Wyszukaj przepis' />
-            <button>Szukaj</button>
-            {recipe.map((element,index)=>(
+            <input type="text" placeholder='Wyszukaj przepis' onChange={(event)=>setRecipeName(event.target.value)}/>
+            <button onClick={()=>SearchFood()}>Szukaj</button>
+            {recipes.map((element,index)=>(
                 <div className='SingleRecipeContainer' key={index}>
-                    <img src={pizza}alt="" />
-                    <h1 id="Name">{element} </h1>
+                    <div style={{backgroundImage:`url(${element.image})`}}className='DishImage'></div>
+                    <h1 id="Name">{element.title} </h1>
 
                     <div className='DescriptionContainer'>
                         <h3 id="Description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti voluptatibus debitis veniam, consequatur, suscipit blanditiis odio porro ad, earum error sequi nobis dicta a aperiam mollitia nihil doloremque aspernatur! Itaque.</h3>
